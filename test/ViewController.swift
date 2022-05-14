e7dd358206caa15a611c5ed1aee39a166dcc5782
Haydar Bekmuradov <haydarbekmuradov50@gmail.com>
2022-05-14 15:22:42 +0300

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Haydar Bekmuradov on 14.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
